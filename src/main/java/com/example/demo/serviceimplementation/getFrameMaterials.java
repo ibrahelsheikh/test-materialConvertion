23ed3514f7f6db3232f7958957f13168c88cab9e
Ibrahim Elsheikh <ibrahimelsheik2020@gmail.com>
2025-11-09 10:28:31 +0200

    data frame

---
+++ src/main/java/com/example/demo/serviceimplementation/getFrameMaterials.java
@@ -0,0 +1,17 @@
+package com.example.demo.serviceimplementation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class getFrameMaterials {
+    public static List<Material> getFrameMaterials (String DOOR_LENGTH,
+                                                    String DOOR_WIDTH ,
+                                                    String FRAME_TYPE ,
+                                                    String FRAME_MOUNTING_TYPE,
+                                                    int FRAME_THICKNESS){
+
+        List<Material> materialArrayList = new ArrayList<>();
+
+        return materialArrayList;
+    }
+}
